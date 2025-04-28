153a1e56fec9bdd87ff40d9d47cbc66504334b0d
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 15:57:38 -0700

    feat:Product repository added

--- src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -1,4 +1,19 @@
 package com.ecom.productcatalog.controller;
 
+import com.ecom.productcatalog.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+
+    }
 }
