629294ea8965105a9513a432e3dfec5eb01669bc
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 15:26:46 -0700

    feat: Category repository added

--- src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -1,4 +1,14 @@
 package com.ecom.productcatalog.controller;
 
+import com.ecom.productcatalog.model.Category;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    public List<Category> getAllCategories() {
+
+    }
 }
