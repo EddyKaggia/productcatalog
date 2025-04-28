e56cdab239b3d2e3a73c96b29fbd03997cc86d3e
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 16:03:37 -0700

    feat: Product service refactored

--- src/main/java/com/ecom/productcatalog/service/ProductService.java
+++ src/main/java/com/ecom/productcatalog/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.ecom.productcatalog.service;
 
+import com.ecom.productcatalog.model.Product;
+import com.ecom.productcatalog.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
