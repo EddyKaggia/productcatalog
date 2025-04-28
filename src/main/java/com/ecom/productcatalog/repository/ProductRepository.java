7df1a46942a7b068fbe078eee7d03edcd0f6f501
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 15:59:07 -0700

    feat:Product repository refactored

--- src/main/java/com/ecom/productcatalog/repository/ProductRepository.java
+++ src/main/java/com/ecom/productcatalog/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecom.productcatalog.repository;
 
 import com.ecom.productcatalog.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
