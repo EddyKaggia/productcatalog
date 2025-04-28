a58807a9bea74040d46e294fb97e7eda46a58194
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 15:29:55 -0700

    feat: Category controller refactored

--- src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -4,11 +4,12 @@ import com.ecom.productcatalog.model.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
 
     public List<Category> getAllCategories() {
-
     }
 }
