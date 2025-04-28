7a112fd01ed2e0bea61c71abd1afffd7c236f3d2
EddyKaggia <kagzcodes@gmail.com>
2025-04-28 16:39:33 -0700

    feat:Config data seeder file refactored

--- src/main/java/com/ecom/productcatalog/config/DataSeeder.java
+++ src/main/java/com/ecom/productcatalog/config/DataSeeder.java
@@ -1,4 +1,72 @@
 package com.ecom.productcatalog.config;
 
-public class DataSeeder {
+import com.ecom.productcatalog.model.Category;
+import com.ecom.productcatalog.model.Product;
+import com.ecom.productcatalog.repository.CategoryRepository;
+import com.ecom.productcatalog.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public DataSeeder(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String ... args) throws Exception {
+        // Clear all existing data
+        productRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        // Create Categories
+        Category electronics = new Category();
+        electronics.setName("Electronics");
+
+        Category clothing = new Category();
+        electronics.setName("Clothing");
+
+        Category home = new Category();
+        electronics.setName("Home and Kitchen");
+
+        categoryRepository.saveAll(Arrays.asList(electronics, home, clothing));
+
+        //Create Products
+        Product phone = new Product();
+        phone.setName("Smartphone");
+        phone.setDescription("Latest, fully speced out smartphone");
+        phone.setImageUrl("");
+        phone.setPrice(1499.99);
+        phone.setCategory(electronics);
+
+        Product laptop = new Product();
+        laptop.setName("Laptop");
+        laptop.setDescription("High performance");
+        laptop.setImageUrl("");
+        laptop.setPrice(2999.99);
+        laptop.setCategory(electronics);
+
+        Product jacket = new Product();
+        jacket.setName("Bomber");
+        jacket.setDescription("Camo, green");
+        jacket.setImageUrl("");
+        jacket.setPrice(129.99);
+        jacket.setCategory(clothing);
+
+        Product blender = new Product();
+        blender.setName("Blender");
+        blender.setDescription("Juicer, smoothie, puree");
+        blender.setImageUrl("");
+        blender.setPrice(59.99);
+        blender.setCategory(home);
+
+        productRepository.saveAll(Arrays.asList(phone, laptop, jacket));
+    }
+
 }
