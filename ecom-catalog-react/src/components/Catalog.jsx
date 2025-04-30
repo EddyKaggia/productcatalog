5ae248452b73d5c5690e9dc7a4d29f695560b347
EddyKaggia <kagzcodes@gmail.com>
2025-04-30 16:46:50 -0700

    feat:Catalog component refactored

--- ecom-catalog-react/src/components/Catalog.jsx
+++ ecom-catalog-react/src/components/Catalog.jsx
@@ -4,7 +4,16 @@ const Catalog = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/api/products`);
+    async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/products`);
+
+        const data = response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
   }, []);
 
   return <div></div>;
